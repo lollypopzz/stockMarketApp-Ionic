03cc8e25e54d0e5f5c9f8b81252a9225855e0101
noonnieenunn <nutthanich.s@burda.co.th>
2016-12-13 16:02:17 +0700

    Finished: Style Stock View

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -88,6 +88,13 @@ angular.module('stockMarketApp.controllers', [])
       promise.then(function(data) {
         console.log(data);
         $scope.stockPriceData = data;
+
+        if(data.chg_percent >= 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#33cd5f'};
+        }
+        else if(data.chg_percent < 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#ef473a'};
+        }
       });
     }
 
@@ -154,7 +161,7 @@ angular.module('stockMarketApp.controllers', [])
   	$scope.chartOptions = {
       chartType: 'linePlusBarWithFocusChart',
       data: 'myData',
-      margin: {top: 15, right: 40, bottom: marginBottom, left: 70},
+      margin: {top: 15, right: 0, bottom: marginBottom, left: 0},
       interpolate: "cardinal",
       useInteractiveGuideline: false,
       yShowMaxMin: false,
@@ -169,7 +176,10 @@ angular.module('stockMarketApp.controllers', [])
       y2AxisTickFormat: y2TickFormat,
       y3AxisTickFormat: y3TickFormat,
       y4AxisTickFormat: y4TickFormat,
-      transitionDuration: 500
+      transitionDuration: 500,
+      y1AxisLabel: 'Price',
+      y3AxisLabel: 'Volume',
+      noDate: 'Loading data...'
   	};
 
 
